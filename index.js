60d482a6364688b2de0e6f4e04842ee639febe37
apieroway <alan.pieroway@thalesgroup.com>
2025-08-10 22:04:01 -0400

    fix: enable CORS to allow API requests from client

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 61571;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
